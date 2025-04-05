ebb44b8b5f5f23915b6389de9d56066197e047bc
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-04-05 21:03:39 +0530

    init

---
+++ src/client/java/org/devaforgestudios/ysws_mcmod/client/Ysws_mcmodClient.java
@@ -0,0 +1,11 @@
+package org.devaforgestudios.ysws_mcmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Ysws_mcmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        println("Hello World");
+    }
+}
---
+++ src/main/java/org/devaforgestudios/ysws_mcmod/Ysws_mcmod.java
@@ -0,0 +1,10 @@
+package org.devaforgestudios.ysws_mcmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ysws_mcmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
